2ce694d334d3e9f4afc7dc50e8eff9de5dc0d9ae
Evan Rutledge Borden <eborden@frontrowed.com>
2017-03-31 17:13:22 -0400

    Experimental draft.

---
+++ Example.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+module Main where
+
+import Lib
+import Data.Functor.Identity (Identity(..))
+import Control.Monad.Free (iterM)
+
+main :: IO ()
+main =
+  -- wrap computation to allow shrinking, saving failed states, replay, etc with a free monad
+  withGraphIO $ do
+    -- Declare the graph at the term level
+    a <- node @A
+    b <- nodeWith @B a -- This can actually be inferred
+    c <- nodeEditWith @C (a, b) $ \n ->
+      n { cc = "spanish" }
+
+    -- do shit with it
+    liftIO $ print (a, b, c)
+
+
+withGraphIO :: Graph Identity r -> IO r
+withGraphIO f = flip iterM f $ \case
+  Insert n next ->
+    next $ Just $ Identity n
+  Validate _ next ->
+    next True
+  LiftIO io next ->
+    next =<< io
+
+
+data A = A { aa :: String, ab :: Int }
+  deriving (Show)
+
+instance Arbitrary A where
+  arbitrary = A <$> arbitrary <*> arbitrary
+
+instance HasDependencies A where
+  type Dependencies A = ()
+  dependsOn = const
+
+
+data B = B { ba :: A, bb :: String }
+  deriving (Show)
+
+instance Arbitrary B where
+  arbitrary = B <$> arbitrary <*> arbitrary
+
+instance HasDependencies B where
+  type Dependencies B = Identity A
+  dependsOn b a = b {ba = runIdentity a}
+
+
+data C = C { ca :: A, cb :: B , cc :: String}
+  deriving (Show)
+
+instance Arbitrary C where
+  arbitrary = C <$> arbitrary <*> arbitrary <*> arbitrary
+
+instance HasDependencies C where
+  type Dependencies C = (Identity A, Identity B)
+  dependsOn c (a, b) = C (runIdentity a) (runIdentity b) (cc c)
---
+++ src/Lib.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Lib
+  ( module Lib
+  , Arbitrary(..)
+  ) where
+
+import Test.QuickCheck
+import Control.Monad.Free
+import Control.Exception
+import Data.Proxy
+import Data.Typeable
+
+
+type Graph entity = Free (Actions entity)
+
+
+data Actions entity next where
+  Insert :: a -> (Maybe (entity a) -> next) -> Actions entity next
+  Validate :: a -> (Bool -> next) -> Actions entity next
+  LiftIO :: IO a -> (a -> next) -> Actions entity next
+
+deriving instance Functor (Actions entity)
+
+liftIO :: IO a -> Graph entity a
+liftIO io = liftF (LiftIO io id)
+
+insert :: a -> Graph entity (Maybe (entity a))
+insert n = liftF (Insert n id)
+
+validate :: a -> Graph entity Bool
+validate n = liftF (Validate n id)
+
+
+class HasDependencies a where
+  type Dependencies a
+  dependsOn :: a -> Dependencies a -> a
+
+
+data GenerationFailure =
+  GenerationFailureMaxAttempts TypeRep
+  deriving (Show, Typeable)
+
+instance Exception GenerationFailure
+
+nodeEditWith :: (Typeable a, Arbitrary a, HasDependencies a) => (Dependencies a) -> (a -> a) -> Graph entity (entity a)
+nodeEditWith dependencies edits =
+  tryInsert 10 0 $ do
+    x <- liftIO $ generate arbitrary
+    pure (edits x `dependsOn` dependencies)
+
+nodeWith :: (Typeable a, Arbitrary a, HasDependencies a) => (Dependencies a) -> Graph entity (entity a)
+nodeWith = flip nodeEditWith id
+
+nodeEdit :: (Typeable a, Arbitrary a, HasDependencies a, Dependencies a ~ ()) => (a -> a) -> Graph entity (entity a)
+nodeEdit edits = nodeEditWith () edits
+
+node :: (Typeable a, Arbitrary a, HasDependencies a, Dependencies a ~ ()) => Graph entity (entity a)
+node = nodeWith ()
+
+tryInsert :: forall a entity. Typeable a => Int -> Int -> (Graph entity a) -> Graph entity (entity a)
+tryInsert maxAttempts currentAttempts source
+  | currentAttempts >= maxAttempts =
+      liftIO . throwIO $ GenerationFailureMaxAttempts (typeRep (Proxy :: Proxy a))
+  | otherwise = do
+    value <- source
+    isValid <- validate value
+    if isValid then
+      insert value >>= \case
+        Just a -> pure a
+        Nothing -> tryInsert maxAttempts (succ currentAttempts) source
+    else
+      tryInsert maxAttempts (succ currentAttempts) source
